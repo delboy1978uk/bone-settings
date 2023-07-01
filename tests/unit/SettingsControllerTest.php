06c6b82e1db927987795bf5f794539c4620974f3
Derek Stephen McLean <delboy1978uk@gmail.com>
2023-07-01 17:36:07 +0200

    add unit tests

--- tests/unit/SettingsControllerTest.php
+++ tests/unit/SettingsControllerTest.php
@@ -5,26 +5,41 @@ namespace unit;
 use Bone\Settings\Controller\SettingsController;
 use Codeception\TestCase\Test;
 use Del\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use PHPUnit\Framework\MockObject\MockObject;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class SettingsControllerTest extends Test
 {
+    private SettingsController $settingsController;
+    private MockObject $request;
+
+    public function _before()
+    {
+        $user = $this->createMock(User::class);
+        $user->method('getId')->willReturn(1314);
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request->method('getAttribute')->willReturn($user);
+        $this->request = $request;
+        $repository = $this->createMock(EntityRepository::class);
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $entityManager->method('getRepository')->willReturn($repository);
+        $this->settingsController = new SettingsController($entityManager);
+    }
+
     public function testGetSettings()
     {
-        // we'll need this guy soon
-//        $user = $this->createMock(User::class);
-        $request = $this->createMock(ServerRequestInterface::class);
-        $controller = new SettingsController();
-        $response = $controller->getUserSettings($request);
+        $response = $this->settingsController->getUserSettings($this->request);
         $this->assertInstanceOf(ResponseInterface::class, $response);
     }
 
     public function testUpdateSettings()
     {
-        $request = $this->createMock(ServerRequestInterface::class);
-        $controller = new SettingsController();
-        $response = $controller->updateUserSettings($request);
+        $request = $this->request;
+        $request->method('getParsedBody')->willReturn(['xxx' => true]);
+        $response = $this->settingsController->updateUserSettings($request);
         $this->assertInstanceOf(ResponseInterface::class, $response);
     }
 }
--- tests/unit/SettingsPackageTest.php
+++ tests/unit/SettingsPackageTest.php
@@ -5,8 +5,10 @@ namespace unit;
 use Barnacle\Container;
 use Bone\OAuth2\Http\Middleware\ResourceServerMiddleware;
 use Bone\Router\Router;
+use Bone\Settings\Controller\SettingsController;
 use Bone\Settings\SettingsPackage;
 use Codeception\TestCase\Test;
+use Doctrine\ORM\EntityManagerInterface;
 use League\Route\RouteGroup;
 
 class SettingsPackageTest extends Test
@@ -16,11 +18,13 @@ class SettingsPackageTest extends Test
         $container = new Container();
         $router = $this->createMock(Router::class);
         $container[ResourceServerMiddleware::class] = $this->createMock(ResourceServerMiddleware::class);
+        $container[EntityManagerInterface::class] = $this->createMock(EntityManagerInterface::class);
         $group = $this->createMock(RouteGroup::class);
         $router->expects(self::once())->method('group')->willReturn($group);
         $package = new SettingsPackage();
         $package->addToContainer($container);
         $package->addRoutes($container, $router);
         $this->assertIsString($package->getEntityPath());
+        $this->assertInstanceOf(SettingsController::class, $container->get(SettingsController::class));
     }
 }
